0ae37b4b009d222d2b04dc9586dbeefeb9b7d562
dmdboi <hello@maxdiamond.co.uk>
2022-06-21 19:08:58 +0100

    Added Readme

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 # Deploy NodeJS project with PM2
 user=$1
 ghuser=$2
-project=$3
+repository=$3
 
 su $user
-git clone git@github.com:$ghuser/$project.git
-cd $project
+git clone git@github.com:$ghuser/$repository.git
+cd $repository
 
 // Run install script
 npm install 
